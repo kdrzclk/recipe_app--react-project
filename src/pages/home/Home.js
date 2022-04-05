eaac6a29849ec1c21031dcceb1046ebf408acde2
kdrzclk <kadir.ozcelik16@gmail.com>
2022-04-06 01:37:21 +0300

    Not Found Page done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,11 @@ import { HomeImage, HomeWrapper } from "./style";
 import HomeIcon from "../../assets/home.svg";
 
 const Home = () => {
+  // const APP_ID = "7d780e9c";
+  // const APP_KEY = "cf47a5aca314e0af9e273a1a1c5a035e";
+
+  // const baseURL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&mealType=${meal}`;
+
   return (
     <HomeWrapper>
       {<Header />}
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -5,6 +5,7 @@ import About from "../pages/about/About";
 import Home from "../pages/home/Home";
 import Github from "../pages/github/Github";
 import Login from "../pages/login/Login";
+import NotFound from "../pages/notfound/NotFound";
 
 const AppRouter = () => {
   return (
@@ -17,6 +18,7 @@ const AppRouter = () => {
           <Route path="/about" element={<About />} />
           <Route path="/github" element={<Github />} />
           <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
