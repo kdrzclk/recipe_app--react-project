3ab48e508cd58abb900a76b8114789984769654d
kdrzclk <kadir.ozcelik16@gmail.com>
2022-04-07 14:57:17 +0300

    finished

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { AboutImage, AboutWrapper, AboutTitle, AboutText } from "./style";
 import Coding from "../../assets/coding.svg";
 
